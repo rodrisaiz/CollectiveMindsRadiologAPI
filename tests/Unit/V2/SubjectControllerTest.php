6e729d7b7fa1ab8c35cbefdcbcba80d2c7504690
Rodrigo Saiz <rodrigo.saiz@somosinapsis.com>
2024-11-23 16:58:30 +0100

    Adding tests for actions

--- tests/Unit/V2/SubjectControllerTest.php
+++ tests/Unit/V2/SubjectControllerTest.php
@@ -24,20 +24,69 @@ class SubjectControllerTest extends V1SubjectControllerTest
 
     public function test_webhook_subject_created()
     {
-        //$webhookUrl = 'https://example.com/webhook';
-        $webhookUrl = config('services.webhook.subject');
+        $webhookUrl = 'https://example.com/webhook';
+        \Config::set('services.webhook.subject', $webhookUrl);
         
         \Http::fake();
 
         $subject = Subject::factory()->create();
-        event(new \App\Http\V2\Events\SubjectEvent($subject));
+        $action = "subject test created";
 
-        \Http::assertSent(function ($request) use ($webhookUrl, $subject) {
+        event(new \App\Http\V2\Events\SubjectEvent($subject, $action));
+
+        \Http::assertSent(function ($request) use ($webhookUrl, $subject, $action) {
             return $request->url() === $webhookUrl &&
+                   $request['action'] === $action;
                    $request['id'] === $subject->id;
         });
     }
 
+    public function test_webhook_subject_updated()
+    {
+        $webhookUrl = 'https://example.com/webhook';
+        \Config::set('services.webhook.subject', $webhookUrl);
+        
+        \Http::fake();
+
+        $subject = Subject::factory()->create([
+            'first_name' => 'Original Name',
+        ]);
+
+        $subject->update([
+            'first_name' => 'Updated Name',
+        ]);
+
+        $action = "subject test updated";
+        event(new \App\Http\V2\Events\SubjectEvent($subject, $action));
+
+        \Http::assertSent(function ($request) use ($webhookUrl, $subject, $action) {
+            return $request->url() === $webhookUrl &&
+                   $request['action'] === $action;
+                   $request['id'] === $subject->id;
+        });
+    }
+
+    public function test_webhook_subject_deleted()
+    {
+        $webhookUrl = 'https://example.com/webhook';
+        \Config::set('services.webhook.subject', $webhookUrl);
+        
+        \Http::fake();
+
+        $subject = Subject::factory()->create([
+            'first_name' => 'Original Name',
+        ]);
+
+        $action = "subject test deleted";
+        event(new \App\Http\V2\Events\SubjectEvent($subject, $action));
+        $subject->delete();
+
+        \Http::assertSent(function ($request) use ($webhookUrl, $subject, $action) {
+            return $request->url() === $webhookUrl &&
+                   $request['action'] === $action;
+                   $request['id'] === $subject->id;
+        });
+    }
 
         
 }
