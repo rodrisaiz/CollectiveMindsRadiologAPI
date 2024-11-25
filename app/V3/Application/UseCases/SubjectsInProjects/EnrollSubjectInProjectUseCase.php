1c1c4a1f3e1e75a3fb3a326e2ef53ae7e9c84a49
Rodrigo Saiz <rodrigo.saiz@somosinapsis.com>
2024-11-25 19:49:59 +0100

    Webhook fully working for subjects in projects

--- app/V3/Application/UseCases/SubjectsInProjects/EnrollSubjectInProjectUseCase.php
+++ app/V3/Application/UseCases/SubjectsInProjects/EnrollSubjectInProjectUseCase.php
@@ -4,19 +4,29 @@ namespace App\V3\Application\UseCases\SubjectsInProjects;
 
 use App\V3\Domain\Repositories\SubjectsInProjectsRepositoryInterface;
 use Illuminate\Support\Facades\Log;
+use App\V3\Domain\Contracts\EventInterface;
+use App\V3\Domain\Contracts\WebhookInterface;
 
 class EnrollSubjectInProjectUseCase
 {
     private SubjectsInProjectsRepositoryInterface $repository;
 
-    public function __construct(SubjectsInProjectsRepositoryInterface $repository)
+    private $eventService;
+    private $webhookService;
+
+    public function __construct(SubjectsInProjectsRepositoryInterface $repository, EventInterface $eventService, WebhookInterface $webhookService)
     {
         $this->repository = $repository;
+        $this->eventService = $eventService;
+        $this->webhookService = $webhookService;
     }
 
     public function execute(int $subjectId, int $projectId)
     {
         $value = $this->repository->enroll($subjectId, $projectId);
+
+        $this->eventService->dispatch($value, 'Subject enrolled in a project');
+        $this->webhookService->send('subjectV3', 'Subject enrolled in a project', $subjectId);
 
         return $value;
     }
