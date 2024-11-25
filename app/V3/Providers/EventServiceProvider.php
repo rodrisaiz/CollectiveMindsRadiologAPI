15944540adaff7b2430e2ccc226fe290cfcb06a3
Rodrigo Saiz <rodrigo.saiz@somosinapsis.com>
2024-11-25 20:26:20 +0100

    Refactoring tests

--- app/V3/Providers/EventServiceProvider.php
+++ app/V3/Providers/EventServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\V3\Providers;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
 use App\V3\Domain\Events\BaseEvent;
 use App\V3\Infrastructure\Listeners\SendSubjectWebhookListener;
+use App\V3\Infrastructure\Listeners\SendProjectWebhookListener;
 use App\V3\Domain\Events\SubjectEnrolledInProjectEvent;
 use App\V3\Infrastructure\Listeners\SendSubjectEnrolledWebhook;
 
@@ -13,6 +14,7 @@ class EventServiceProvider extends ServiceProvider
     protected $listen = [
         BaseEvent::class => [
             SendSubjectWebhookListener::class,
+            SendProjectWebhookListener::class,
         ],
     ];
 }
